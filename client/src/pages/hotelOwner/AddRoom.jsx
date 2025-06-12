d41614d942b2f76d5ee4bb7d238e7e812c6660ea
virpariyamalay <mpcvirpariya@gmail.com>
2025-06-12 19:21:10 +0530

    admin side's dashboard page complete

--- client/src/pages/hotelOwner/AddRoom.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddRoom = () => {
-  return (
-    <div>AddRoom</div>
-  )
-}
-
-export default AddRoom
--- client/src/pages/hotelOwner/ListRoom.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ListRoom = () => {
-  return (
-    <div>ListRoom</div>
-  )
-}
-
-export default ListRoom
