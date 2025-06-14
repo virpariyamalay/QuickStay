e53261054f219435aff7cfefecd72297e210e856
virpariyamalay <mpcvirpariya@gmail.com>
2025-06-14 17:18:22 +0530

    api created for room ownerroom and toggle-availability

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import upload from '../middleware/uploadMiddleware.js';
-import { protect } from '../middleware/authMiddleware,js';
-import { createRoom } from '../controllers/roomController.js';
+import { protect } from '../middleware/authMiddleware.js';
+import { createRoom, getOwnerRooms, getRoom, toggleRoomAvailability } from '../controllers/roomController.js';
+
 
 const roomRouter = express.Router();
 
+
 roomRouter.post('/',upload.array("images",4),protect,createRoom);
+roomRouter.get('/',getRoom);
+roomRouter.get('/owner',protect,getOwnerRooms);
+roomRouter.post('/toggle-availability',protect,toggleRoomAvailability);
+
+
+
+
+
 export default roomRouter;
