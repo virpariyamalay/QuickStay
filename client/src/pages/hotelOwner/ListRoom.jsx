bcfa8a3ffa12890b88ab6df9d284843affe9cc8b
virpariyamalay <mpcvirpariya@gmail.com>
2025-06-16 14:37:40 +0530

    toggle availability is successfully done

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -16,7 +16,7 @@ const ListRoom = () => {
         { headers: { Authorization: `Bearer ${await getToken()}` } })
       if (data.success) {
         setRooms(data.rooms)
-        
+
       } else {
         toast.error(data.message);
 
@@ -26,11 +26,26 @@ const ListRoom = () => {
 
     }
   }
-  useEffect(()=>{
-    if(user){
+
+  //toggle-avalibility of room
+  const toggleAvailability = async (roomId) => {
+    const { data } = await axios.post('/api/rooms/toggle-availability', { roomId },
+      { headers: { Authorization: `Bearer ${await getToken()}` } })
+    if (data.success) {
+      toast.success(data.message)
+      fetchRooms()
+    } else {
+      toast.error(data.message);
+
+    }
+  }
+
+
+  useEffect(() => {
+    if (user) {
       fetchRooms()
     }
-  },[user])
+  }, [user])
 
   return (
     <div>
@@ -64,7 +79,7 @@ const ListRoom = () => {
 
                 <td className='py-3 px-4 border-t border-gray-300 text-sm text-red-500 text-center'>
                   <label className='relative inline-flex item-center cursor-pointer text-gray-900 gap-3'>
-                    <input type="checkbox" className='sr-only peer' checked={item.isAvailable} />
+                    <input onChange={()=>toggleAvailability(item._id)} type="checkbox" className='sr-only peer' checked={item.isAvailable} />
                     <div className='w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200'></div>
                     <span className='dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5'></span>
                   </label>
