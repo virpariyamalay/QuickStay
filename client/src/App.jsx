749028d112a61876f468009d120beba25f680f54
virpariyamalay <mpcvirpariya@gmail.com>
2025-06-18 20:17:20 +0530

    error solved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import RoomDetails from './pages/RoomDetails';
 import MyBookings from './pages/MyBookings';
 import HotelReg from './components/HotelReg';
 import Layout from './pages/hotelOwner/Layout';
-import DashBoard from './pages/hotelOwner/DashBoard';
+import Dashboard from './pages/hotelOwner/Dashboard';
 import AddRoom from './pages/hotelOwner/AddRoom';
 import ListRoom from './pages/hotelOwner/ListRoom';
 import {Toaster} from 'react-hot-toast'
@@ -30,7 +30,7 @@ const App = () => {
         <Route path="/rooms/:id" element={<RoomDetails />} />
         <Route path="/my-bookings" element={<MyBookings />} />
         <Route path="/owner" element={<Layout/>}>
-            <Route index element={<DashBoard/>}/>
+            <Route index element={<Dashboard/>}/>
             <Route path='add-room' element={<AddRoom/>}/>
             <Route path='list-room' element={<ListRoom/>}/>
 
