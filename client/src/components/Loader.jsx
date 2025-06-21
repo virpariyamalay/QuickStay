b9ce8f9c185f1dc8aefec49baa0c6b9de976e033
virpariyamalay <mpcvirpariya@gmail.com>
2025-06-21 14:49:34 +0530

    loader position fixed

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,27 +1,51 @@
+// import React, { useEffect } from 'react'
+// import { useAppContext } from '../context/AppContext'
+// import { useParams } from 'react-router-dom'
+
+// const Loader = () => {
+//     const {navigate}= useAppContext()
+//     const {nextUrl}=useParams()
+
+//     useEffect(()=>{
+//         if(nextUrl){
+//             setTimeout(()=>{
+//                 navigate(`/${nextUrl}`)
+//             },8000)
+//         }
+//     },[nextUrl])
+
+//   return (
+//     <div className='flex justify-center item-center h-screen'>
+//         <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-primary'>
+
+//         </div>
+
+//     </div>
+//   )
+// }
+
+// export default Loader
 import React, { useEffect } from 'react'
 import { useAppContext } from '../context/AppContext'
 import { useParams } from 'react-router-dom'
 
 const Loader = () => {
-    const {navigate}= useAppContext()
-    const {nextUrl}=useParams()
+    const { navigate } = useAppContext()
+    const { nextUrl } = useParams()
 
-    useEffect(()=>{
-        if(nextUrl){
-            setTimeout(()=>{
+    useEffect(() => {
+        if (nextUrl) {
+            setTimeout(() => {
                 navigate(`/${nextUrl}`)
-            },8000)
+            }, 8000)
         }
-    },[nextUrl])
+    }, [nextUrl])
 
-  return (
-    <div className='flex justify-center item-center h-screen'>
-        <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-primary'>
-
+    return (
+        <div className='flex justify-center items-center h-screen w-full'>
+            <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-primary'></div>
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Loader
