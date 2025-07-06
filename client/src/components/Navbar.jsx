1d0687274618ecc204c9daeaef7f53d6517bdf0b
virpariyamalay <mpcvirpariya@gmail.com>
2025-07-06 15:30:22 +0530

    search icon removed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -82,7 +82,7 @@ const Navbar = () => {
 
             {/* Desktop Right */}
             <div className="hidden md:flex items-center gap-4">
-                <img src={assets.searchIcon} alt="search" className={`${isScrolled && 'invert'} h-7 transition-all duration-500`} />
+                
                 {user ?
                     (<UserButton>
                         <UserButton.MenuItems>
